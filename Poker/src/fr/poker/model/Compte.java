1588435b066bd8574ef74d0d40d3037eab80e9ae
Rayan ROGAI <rayan.rogai@hotmail.fr>
2016-11-24 17:52:14 +0100

    [refactor and clean] Compte et joueur + test joueur [delete] mise.java

--- Poker/src/fr/poker/model/Compte.java
+++ Poker/src/fr/poker/model/Compte.java
@@ -1,35 +1,31 @@
 package fr.poker.model;
-/***********************************************************************
- * Module:  Compte.java
- * Author:  Rayan
- * Purpose: Defines the Class Compte
- ***********************************************************************/
 
 import java.util.*;
 
-/** @pdOid 8902f8c9-f17e-4df7-8098-2aec67d74285 */
 public class Compte {
-	/** @pdOid 5ec4455c-bc6d-476e-8bee-4c53f9b49756 */
 	private int id;
-	/** @pdOid 28e6ef37-bcc7-4348-aa48-1016312a1c47 */
 	private String pseudo;
-	/** @pdOid 89dc4d5f-925f-4aa5-b995-df070b561fac */
 	private String nom;
-	/** @pdOid 82001975-bef7-4e0b-b4a6-b519361d5a16 */
 	private String prenom;
-	/** @pdOid 564c8461-4f5b-4167-96f4-52cd4ebc17ac */
 	private String mail;
-	/** @pdOid 8ef7913c-320e-48e9-9031-463eb921cbfa */
 	private int tel;
-	/** @pdOid 2872dcf1-66d0-4492-a534-71628307821a */
 	private int hash;
-	/** @pdOid 1358fb7a-9b9c-4fe4-bb00-d42824686eb7 */
 	private int credit;
-	/** @pdOid 34249d47-6e4d-4fbd-8b36-269f80caa2a5 */
 	private String bio;
-
-	/** @pdRoleInfo migr=no name=Joueur assc=association87 mult=0..1 */
 	public Joueur joueur;
+
+	public Compte(int id, String pseudo, String nom, String prenom, String mail, int tel, int hash, int credit,
+			String bio) {
+		this.id = id;
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.mail = mail;
+		this.tel = tel;
+		this.hash = hash;
+		this.credit = credit;
+		this.bio = bio;
+	}
 
 	public int getId() {
 		return id;
@@ -103,71 +99,19 @@ public class Compte {
 		this.bio = bio;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((bio == null) ? 0 : bio.hashCode());
-		result = prime * result + credit;
-		result = prime * result + hash;
-		result = prime * result + id;
-		result = prime * result + ((joueur == null) ? 0 : joueur.hashCode());
-		result = prime * result + ((mail == null) ? 0 : mail.hashCode());
-		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
-		result = prime * result + ((prenom == null) ? 0 : prenom.hashCode());
-		result = prime * result + ((pseudo == null) ? 0 : pseudo.hashCode());
-		result = prime * result + tel;
-		return result;
+	public Joueur getJoueur() {
+		return joueur;
+	}
+
+	public void setJoueur(Joueur joueur) {
+		this.joueur = joueur;
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Compte other = (Compte) obj;
-		if (bio == null) {
-			if (other.bio != null)
-				return false;
-		} else if (!bio.equals(other.bio))
-			return false;
-		if (credit != other.credit)
-			return false;
-		if (hash != other.hash)
-			return false;
-		if (id != other.id)
-			return false;
-		if (joueur == null) {
-			if (other.joueur != null)
-				return false;
-		} else if (!joueur.equals(other.joueur))
-			return false;
-		if (mail == null) {
-			if (other.mail != null)
-				return false;
-		} else if (!mail.equals(other.mail))
-			return false;
-		if (nom == null) {
-			if (other.nom != null)
-				return false;
-		} else if (!nom.equals(other.nom))
-			return false;
-		if (prenom == null) {
-			if (other.prenom != null)
-				return false;
-		} else if (!prenom.equals(other.prenom))
-			return false;
-		if (pseudo == null) {
-			if (other.pseudo != null)
-				return false;
-		} else if (!pseudo.equals(other.pseudo))
-			return false;
-		if (tel != other.tel)
-			return false;
-		return true;
+	public String toString() {
+		return "Compte [id=" + id + ", pseudo=" + pseudo + ", nom=" + nom + ", prenom=" + prenom + ", mail=" + mail
+				+ ", tel=" + tel + ", hash=" + hash + ", credit=" + credit + ", bio=" + bio + ", joueur=" + joueur
+				+ "]";
 	}
 
 }
