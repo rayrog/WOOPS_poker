f2ebd39e4b291bcb3bf7bb6cfe84c2b03bd6d14a
Quentin Fleury <quent.fleury@gmail.com>
2016-12-05 08:27:39 +0100

    Add Vjeu

---
+++ Poker/src/fr/poker/view/Vjeu.java
@@ -0,0 +1,43 @@
+package fr.poker.view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Vjeu {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Vjeu window = new Vjeu();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Vjeu() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1300, 800);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
