e99e3060d4c5dfa1c6d769b1c3975bc1ff885a8d
Rayan ROGAI <rayan.rogai@hotmail.fr>
2016-12-05 09:59:35 +0100

    [Refactor] clean and correct

--- Poker/src/fr/poker/controller/Cpartie.java
+++ Poker/src/fr/poker/controller/Cpartie.java
@@ -1,50 +1,35 @@
 package fr.poker.controller;
-import java.awt.List;
-// cette class va nous permettre de repartir les roles
-import java.io.BufferedReader;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Collection;
 
-import javax.swing.JOptionPane;
-//import fr.poker.vue; // mettre l'import du bon IHM
-import fr.poker.controller.Clanceur;
+import java.util.*;
+import fr.poker.model.*;
 
-import fr.poker.model.Carte;
-import fr.poker.model.Paquet;
-import fr.poker.model.Joueur;
-
-public class Cpartie extends Thread {
+public class Cpartie {
 
 	ArrayList<Joueur> mesJoueurs = new ArrayList<Joueur>();
-	
+
 	public Cpartie(ArrayList<Joueur> Joueurs) {
 		this.mesJoueurs = Joueurs;
 	}
+
 	public void run() {
-	
-		for (int i=0; i < mesJoueurs.size(); i++){	// Je parcours ma liste de joueur
-			System.out.println("Je suis actuellement sur ce joueur: "+Joueur.get(compte));
-			if(i==0){
-				mesJoueurs.setAttribute(role,"dealeur");
+		
+		for (int i = 0; i < mesJoueurs.size(); i++) {
+			Joueur currentJ = mesJoueurs.get(i);
+			System.out.println("Je suis actuellement sur ce joueur: " + currentJ);
+			switch (i) {
+			case 1:
+				currentJ.setRole("Dealer");
+				break;
+			case 2:
+				currentJ.setRole("Petite blinde");
+				break;
+			case 3:
+				currentJ.setRole("Grosse blinde");
+				break;
+			default:
+				currentJ.setRole("Neutre");
 			}
-			else{
-				if(i==1){
-					mesJoueurs.setAttribute(role,"petite blind");
-				}
-				else{
-					if(i==2){
-						mesJoueurs.setAttribute(role,"grosse blind");
-					}
-					else{		// Sinon ils n'ont pas de roles les gens
-						mesJoueurs.setAttribute(role,"pas de role");
-					}
-				}
-			}
-			
-			
 		}
-		// Il faut des getters et setters
 	}
-	
+
 }
